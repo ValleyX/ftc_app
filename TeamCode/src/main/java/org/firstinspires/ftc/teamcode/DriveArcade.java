8fcde043831519319670c162c5efbb2380141cfd
Jonathon <madderom@cox.net>
2018-08-24 15:34:50 -0700

    Update with prints

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveArcade.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveArcade.java
@@ -12,6 +12,10 @@ public class DriveArcade extends LinearOpMode
 {
     DcMotor leftMotor, rightMotor;
     float   leftPower, rightPower, xValue, yValue;
+    private Servo armServo;
+
+    private static final double ARM_RETRACTED_POSITION = 0.4;
+    private static final double ARM_EXTENDED_POSITION = 0.6;
 
     @Override
     public void runOpMode() throws InterruptedException
@@ -21,15 +25,23 @@ public class DriveArcade extends LinearOpMode
 
         leftMotor.setDirection(DcMotor.Direction.REVERSE);
 
+        armServo = hardwareMap.servo.get("servo1");
+        armServo.setPosition(ARM_RETRACTED_POSITION);
+
         telemetry.addData("Mode", "waiting");
         telemetry.update();
 
+        System.out.println("ValleyX: Waiting for Start");
         waitForStart();
+        System.out.println("ValleyX: Starting...");
 
         while (opModeIsActive())
         {
-            yValue = gamepad1.right_stick_y;
-            xValue = gamepad1.right_stick_x;
+
+            //yValue = -gamepad1.right_stick_y;
+            //xValue = -gamepad1.right_stick_x;
+            yValue = -gamepad1.left_stick_y;
+            xValue = -gamepad1.left_stick_x;
 
             leftPower =  yValue - xValue;
             rightPower = yValue + xValue;
@@ -41,6 +53,18 @@ public class DriveArcade extends LinearOpMode
             telemetry.addData("stick", "  y=" + yValue + "  x=" + xValue);
             telemetry.addData("power", "  left=" + leftPower + "  right=" + rightPower);
             telemetry.update();
+
+            if (gamepad1.a)
+            {
+                armServo.setPosition(ARM_RETRACTED_POSITION);
+                System.out.println("ValleyX: GamePad a");
+            }
+            if (gamepad1.b)
+            {
+                armServo.setPosition(ARM_EXTENDED_POSITION);
+                System.out.println("ValleyX: GamePad b");
+            }
+
 
             idle();
         }
