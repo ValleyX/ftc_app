1bf39d0eb0347da76a42fad510434115b86cba35
PhoenixHCABE <gbros900@gmail.com>
2018-11-27 21:29:39 -0700

    test distance sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamCode/TestOds.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamCode/TestOds.java
@@ -1,15 +1,56 @@
 package org.firstinspires.ftc.teamcode.TeamCode;
 
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+@Autonomous(name="Robot: Test Ods", group="TeamCode")
+@Disabled
 public class TestOds extends LinearOpMode
 {
+    private DistanceSensor sensorRange;
     OpticalDistanceSensor ods;
 
     @Override
-    public void runOpMode()
+    public void runOpMode() throws InterruptedException
     {
+
+        // you can use this as a regular DistanceSensor.
+        //sensorRange = hardwareMap.get(DistanceSensor.class, "sensor_range");
+        sensorRange = hardwareMap.get(DistanceSensor.class, "ods");
+
+
+        // you can also cast this to a Rev2mDistanceSensor if you want to use added
+        // methods associated with the Rev2mDistanceSensor class.
+        Rev2mDistanceSensor sensorTimeOfFlight = (Rev2mDistanceSensor)sensorRange;
+
+        telemetry.addData(">>", "Press start to continue");
+        telemetry.update();
+
+        waitForStart();
+        while(opModeIsActive()) {
+            // generic DistanceSensor methods.
+            telemetry.addData("deviceName",sensorRange.getDeviceName() );
+            telemetry.addData("range", String.format("%.01f mm", sensorRange.getDistance(DistanceUnit.MM)));
+            telemetry.addData("range", String.format("%.01f cm", sensorRange.getDistance(DistanceUnit.CM)));
+            telemetry.addData("range", String.format("%.01f m", sensorRange.getDistance(DistanceUnit.METER)));
+            telemetry.addData("range", String.format("%.01f in", sensorRange.getDistance(DistanceUnit.INCH)));
+
+            // Rev2mDistanceSensor specific methods.
+            telemetry.addData("ID", String.format("%x", sensorTimeOfFlight.getModelID()));
+            telemetry.addData("did time out", Boolean.toString(sensorTimeOfFlight.didTimeoutOccur()));
+
+            telemetry.update();
+        }
+    }
+
+/*
+
         ods = hardwareMap.opticalDistanceSensor.get("ods");
         ods.enableLed(true);
 
@@ -23,4 +64,5 @@ public class TestOds extends LinearOpMode
             telemetry.update();
         }
     }
+*/
 }
